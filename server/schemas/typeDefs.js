a89e2301d17369ed916abd8ad8be381e94eb329f
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-25 12:38:29 -0400

    typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require("apollo-server-express");
+
+// Query, User, Book, Auth, Mutation
+
+const typeDefs = gql`
+    type Query{
+        me: User
+    }
+
+    type User{
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String   
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+        removeBook(bookId: String!): User
+    }
+
+}`;
+
+module.exports = typeDefs;
